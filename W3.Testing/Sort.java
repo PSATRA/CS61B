89c6c04c03795375651bf461672dad7b0b48a75e
PSATRA <2542686176@qq.com>
2023-08-31 14:06:46 +0800

    w3 done

--- W3.Testing/Sort.java
+++ W3.Testing/Sort.java
@@ -1,8 +1,55 @@
 public class Sort {
-    /** Sorts strings destructively. */
+    /**
+     * @NOTICE: Create the input and output sample and test before writing a single method. Use java visualizer to test.
+     * Selection sort algorithm:
+     * 1. find the smallest item
+     * 2. move it to the front
+     * 3. selection sort the rest (using recursion?)
+     */
+
+    /** Returns the smallest string in x.
+     * @source Got help with string compares from https://goo.gl/a7yBU5. */
+    public static int findSmallest(String[] x, int start) {
+        int smallestIndex = start;
+        for (int i = start; i < x.length; i += 1) {
+            int cmp = x[i].compareTo(x[smallestIndex]); // Here
+            if (cmp < 0) {
+                smallestIndex = i;
+            }
+        }
+        return smallestIndex;
+    }
+
+    public static void swap(String[] x, int a, int b) {
+        String temp = x[a];
+        x[a] = x[b];
+        x[b] = temp;
+    }
+
+    /**
+     * @NOTICE: A typical solution is to create a private helper method that has an
+     *  additional parameter (or parameters) that delineate which part of the array to consider.
+     *  This approach is quite common when trying to use recursion on a data structure that is
+     *  not inherently recursive, e.g. arrays.
+     * @param x: the data structure that is not inherently recursive.
+     * @param start: delineate which part of the array to consider, here it is an index.
+     */
+    public static void sort(String[] x, int start) {
+        // Helper method: sorts strings destructively starting from item start.
+        if (start == x.length) {
+            return;
+        }
+        int smallestIndex = findSmallest(x, start);
+        swap(x, start, smallestIndex);
+        sort(x, start + 1);
+        /**
+         * Doesn't need to increment start, it's similarly done by parameter passing.
+         * As is shown in the compiler, start: start + 1.
+         */
+    }
+
+    /** Final result. */
     public static void sort(String[] x) {
-        // find the smallest item
-        // move it to the front
-        // selection sort the rest (using recursion?)
+        sort(x, 0);
     }
 }
--- W3.Testing/TestSort.java
+++ W3.Testing/TestSort.java
@@ -1,12 +1,44 @@
+import org.junit.Test; // Replace @org.junit.Test with @Test
+import static org.junit.Assert.*; // Omit the prefix org.junit.Assert.
+
 public class TestSort {
-    public static void testSort() {
+    @Test
+    /**
+     * Run the single test method independently and without main method.
+     * Remember to delete static.
+     * The result is on the left sidebar, green check or red X.
+     */
+    public void testFindSmallest() {
+        String[] input = {"i", "have", "an", "egg"};
+        int expected = 2;
+
+        int actual = Sort.findSmallest(input, 0);
+        assertEquals(expected, actual);
+
+        String[] input2 = {"there", "are", "many", "pigs"};
+        int expected2 = 2;
+
+        int actual2 = Sort.findSmallest(input2, 2);
+        assertEquals(expected2, actual2);
+    }
+
+    @Test
+    public void testSwap() {
+        String[] input = {"i", "have", "an", "egg"};
+        int a = 0;
+        int b = 2;
+        String[] expected = {"an", "have", "i", "egg"};
+
+        Sort.swap(input, a, b);
+        assertArrayEquals(expected, input);
+    }
+
+    @Test
+    public void testSort() {
         String[] input = {"i", "have", "an", "egg"};
         String[] expected = {"an", "egg", "have", "i"};
         Sort.sort(input);
-        org.junit.Assert.assertArrayEquals(expected, input);
+        assertArrayEquals(expected, input);
     }
 
-    public static void main(String[] args) {
-        testSort();
-    }
 }
