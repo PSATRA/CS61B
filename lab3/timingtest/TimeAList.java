2efd6ac3d74c7e92ca76f27e699b4d5c7c801a4b
PSATRA <2542686176@qq.com>
2023-09-04 13:31:21 +0800

    done

--- lab3/timingtest/TimeAList.java
+++ lab3/timingtest/TimeAList.java
@@ -1,5 +1,6 @@
 package timingtest;
 import edu.princeton.cs.algs4.Stopwatch;
+import org.checkerframework.checker.units.qual.A;
 
 /**
  * Created by hug.
@@ -23,5 +24,49 @@ public class TimeAList {
 
     public static void timeAListConstruction() {
         // TODO: YOUR CODE HERE
+        AList<Integer> Ns = new AList<>();
+        AList<Double> times = new AList<>();
+        AList<Integer> tested = new AList<>();
+        Stopwatch sw = new Stopwatch();
+
+        for (int i = 0; i < 128000; i++) {
+            tested.addLast(i);
+            double timeInSeconds = sw.elapsedTime();
+
+            if (i == 999) {
+                Ns.addLast(1000);
+                times.addLast(timeInSeconds);
+            }
+            if (i == 1999) {
+                Ns.addLast(2000);
+                times.addLast(timeInSeconds);
+            }
+            if (i == 3999) {
+                Ns.addLast(4000);
+                times.addLast(timeInSeconds);
+            }
+            if (i == 7999) {
+                Ns.addLast(8000);
+                times.addLast(timeInSeconds);
+            }
+            if (i == 15999) {
+                Ns.addLast(16000);
+                times.addLast(timeInSeconds);
+            }
+            if (i == 31999) {
+                Ns.addLast(32000);
+                times.addLast(timeInSeconds);
+            }
+            if (i == 63999) {
+                Ns.addLast(64000);
+                times.addLast(timeInSeconds);
+            }
+            if (i == 127999) {
+                Ns.addLast(128000);
+                times.addLast(timeInSeconds);
+            }
+        }
+
+        printTimingTable(Ns, times, Ns);
     }
 }
--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -23,6 +23,108 @@ public class TimeSLList {
 
     public static void timeGetLast() {
         // TODO: YOUR CODE HERE
+        AList<Integer> Ns = new AList<>();
+        AList<Double> times = new AList<>();
+        AList<Integer> opCounts = new AList<>();
+        SLList<Integer> tested = new SLList<>();
+        for (int i = 0; i < 8; i++) {
+            opCounts.addLast(10000);
+        }
+
+        for (int i = 0; i < 128000; i++) {
+            tested.addLast(i);
+
+            if (i == 999) {
+                Ns.addLast(1000);
+                Stopwatch sw = new Stopwatch();
+
+                for (int j = 0; j < 10000; j++) {
+                    tested.getLast();
+                }
+
+                double timeInSeconds = sw.elapsedTime();
+                times.addLast(timeInSeconds);
+            }
+            if (i == 1999) {
+                Ns.addLast(2000);
+                Stopwatch sw = new Stopwatch();
+
+                for (int j = 0; j < 10000; j++) {
+                    tested.getLast();
+                }
+
+                double timeInSeconds = sw.elapsedTime();
+                times.addLast(timeInSeconds);
+            }
+            if (i == 3999) {
+                Ns.addLast(4000);
+                Stopwatch sw = new Stopwatch();
+
+                for (int j = 0; j < 10000; j++) {
+                    tested.getLast();
+                }
+
+                double timeInSeconds = sw.elapsedTime();
+                times.addLast(timeInSeconds);
+            }
+            if (i == 7999) {
+                Ns.addLast(8000);
+                Stopwatch sw = new Stopwatch();
+
+                for (int j = 0; j < 10000; j++) {
+                    tested.getLast();
+                }
+
+                double timeInSeconds = sw.elapsedTime();
+                times.addLast(timeInSeconds);
+            }
+            if (i == 15999) {
+                Ns.addLast(16000);
+                Stopwatch sw = new Stopwatch();
+
+                for (int j = 0; j < 10000; j++) {
+                    tested.getLast();
+                }
+
+                double timeInSeconds = sw.elapsedTime();
+                times.addLast(timeInSeconds);
+            }
+            if (i == 31999) {
+                Ns.addLast(32000);
+                Stopwatch sw = new Stopwatch();
+
+                for (int j = 0; j < 10000; j++) {
+                    tested.getLast();
+                }
+
+                double timeInSeconds = sw.elapsedTime();
+                times.addLast(timeInSeconds);
+            }
+            if (i == 63999) {
+                Ns.addLast(64000);
+                Stopwatch sw = new Stopwatch();
+
+                for (int j = 0; j < 10000; j++) {
+                    tested.getLast();
+                }
+
+                double timeInSeconds = sw.elapsedTime();
+                times.addLast(timeInSeconds);
+            }
+            if (i == 127999) {
+                Ns.addLast(128000);
+                Stopwatch sw = new Stopwatch();
+
+                for (int j = 0; j < 10000; j++) {
+                    tested.getLast();
+                }
+
+                double timeInSeconds = sw.elapsedTime();
+                times.addLast(timeInSeconds);
+            }
+        }
+
+        printTimingTable(Ns, times, opCounts);
     }
 
 }
