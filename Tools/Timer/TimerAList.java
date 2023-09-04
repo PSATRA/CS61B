ff195adb01332a5c85e20b8a4d258b65d28698c9
PSATRA <2542686176@qq.com>
2023-09-04 13:32:14 +0800

    done

---
+++ Tools/Timer/TimerAList.java
@@ -0,0 +1,71 @@
+package Timer;
+// import edu.princeton.cs.algs4.Timer.Stopwatch;
+
+/**
+ * Created by hug.
+ */
+public class TimerAList {
+    private static void printTimingTable(AList<Integer> Ns, AList<Double> times, AList<Integer> opCounts) {
+        System.out.printf("%12s %12s %12s %12s\n", "N", "time (s)", "# ops", "microsec/op");
+        System.out.printf("------------------------------------------------------------\n");
+        for (int i = 0; i < Ns.size(); i += 1) {
+            int N = Ns.get(i);
+            double time = times.get(i);
+            int opCount = opCounts.get(i);
+            double timePerOp = time / opCount * 1e6;
+            System.out.printf("%12d %12.2f %12d %12.2f\n", N, time, opCount, timePerOp);
+        }
+    }
+
+    public static void main(String[] args) {
+        timeAListConstruction();
+    }
+
+    public static void timeAListConstruction() {
+        // TODO: YOUR CODE HERE
+        AList<Integer> Ns = new AList<>();
+        AList<Double> times = new AList<>();
+        AList<Integer> tested = new AList<>();
+        Stopwatch sw = new Stopwatch();
+
+        for (int i = 0; i < 128000; i++) {
+            tested.addLast(i);
+            double timeInSeconds = sw.elapsedTime();
+
+            if (i == 999) {
+                Ns.addLast(1000);
+                times.addLast(timeInSeconds);
+            }
+            if (i == 1999) {
+                Ns.addLast(2000);
+                times.addLast(timeInSeconds);
+            }
+            if (i == 3999) {
+                Ns.addLast(4000);
+                times.addLast(timeInSeconds);
+            }
+            if (i == 7999) {
+                Ns.addLast(8000);
+                times.addLast(timeInSeconds);
+            }
+            if (i == 15999) {
+                Ns.addLast(16000);
+                times.addLast(timeInSeconds);
+            }
+            if (i == 31999) {
+                Ns.addLast(32000);
+                times.addLast(timeInSeconds);
+            }
+            if (i == 63999) {
+                Ns.addLast(64000);
+                times.addLast(timeInSeconds);
+            }
+            if (i == 127999) {
+                Ns.addLast(128000);
+                times.addLast(timeInSeconds);
+            }
+        }
+
+        printTimingTable(Ns, times, Ns);
+    }
+}
---
+++ Tools/Timer/TimerSLList.java
@@ -0,0 +1,129 @@
+package Timer;
+
+/**
+ * Created by hug.
+ */
+public class TimerSLList {
+    private static void printTimingTable(AList<Integer> Ns, AList<Double> times, AList<Integer> opCounts) {
+        System.out.printf("%12s %12s %12s %12s\n", "N", "time (s)", "# ops", "microsec/op");
+        System.out.printf("------------------------------------------------------------\n");
+        for (int i = 0; i < Ns.size(); i += 1) {
+            int N = Ns.get(i);
+            double time = times.get(i);
+            int opCount = opCounts.get(i);
+            double timePerOp = time / opCount * 1e6;
+            System.out.printf("%12d %12.2f %12d %12.2f\n", N, time, opCount, timePerOp);
+        }
+    }
+
+    public static void main(String[] args) {
+        timeGetLast();
+    }
+
+    public static void timeGetLast() {
+        // TODO: YOUR CODE HERE
+        AList<Integer> Ns = new AList<>();
+        AList<Double> times = new AList<>();
+        AList<Integer> opCounts = new AList<>();
+        SLList<Integer> tested = new SLList<>();
+        for (int i = 0; i < 8; i++) {
+            opCounts.addLast(10000);
+        }
+
+        for (int i = 0; i < 128000; i++) {
+            tested.addLast(i);
+
+            if (i == 999) {
+                Ns.addLast(1000);
+                Stopwatch sw = new Stopwatch();
+
+                for (int j = 0; j < 10000; j++) {
+                    tested.getLast();
+                }
+
+                double timeInSeconds = sw.elapsedTime();
+                times.addLast(timeInSeconds);
+            }
+            if (i == 1999) {
+                Ns.addLast(2000);
+                Stopwatch sw = new Stopwatch();
+
+                for (int j = 0; j < 10000; j++) {
+                    tested.getLast();
+                }
+
+                double timeInSeconds = sw.elapsedTime();
+                times.addLast(timeInSeconds);
+            }
+            if (i == 3999) {
+                Ns.addLast(4000);
+                Stopwatch sw = new Stopwatch();
+
+                for (int j = 0; j < 10000; j++) {
+                    tested.getLast();
+                }
+
+                double timeInSeconds = sw.elapsedTime();
+                times.addLast(timeInSeconds);
+            }
+            if (i == 7999) {
+                Ns.addLast(8000);
+                Stopwatch sw = new Stopwatch();
+
+                for (int j = 0; j < 10000; j++) {
+                    tested.getLast();
+                }
+
+                double timeInSeconds = sw.elapsedTime();
+                times.addLast(timeInSeconds);
+            }
+            if (i == 15999) {
+                Ns.addLast(16000);
+                Stopwatch sw = new Stopwatch();
+
+                for (int j = 0; j < 10000; j++) {
+                    tested.getLast();
+                }
+
+                double timeInSeconds = sw.elapsedTime();
+                times.addLast(timeInSeconds);
+            }
+            if (i == 31999) {
+                Ns.addLast(32000);
+                Stopwatch sw = new Stopwatch();
+
+                for (int j = 0; j < 10000; j++) {
+                    tested.getLast();
+                }
+
+                double timeInSeconds = sw.elapsedTime();
+                times.addLast(timeInSeconds);
+            }
+            if (i == 63999) {
+                Ns.addLast(64000);
+                Stopwatch sw = new Stopwatch();
+
+                for (int j = 0; j < 10000; j++) {
+                    tested.getLast();
+                }
+
+                double timeInSeconds = sw.elapsedTime();
+                times.addLast(timeInSeconds);
+            }
+            if (i == 127999) {
+                Ns.addLast(128000);
+                Stopwatch sw = new Stopwatch();
+
+                for (int j = 0; j < 10000; j++) {
+                    tested.getLast();
+                }
+
+                double timeInSeconds = sw.elapsedTime();
+                times.addLast(timeInSeconds);
+            }
+        }
+
+        printTimingTable(Ns, times, opCounts);
+    }
+
+}
